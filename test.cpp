b419dd1bbaaeb2ef8b8c8da11b3088ea5a5ec269
Chase-condron <90365135+Chase-condron@users.noreply.github.com>
2021-09-18 09:13:58 -0400

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
 
+int main()
+{
+	double val1, val2, val3, val4, val5;
+	double max;
+	//get 5, print largest value
+	std::cout << "Please enter 5 numbers \n";
+	std::cin >> val1 >> val2 >> val3 >> val4 >> val5;
+
+	if (val1 > val2 && val1 > val3 && val1 > val4 && val1 > val5)
+	{
+		std::cout << "Your largest number is: " << val1;
+	}
+	if (val2 > val1 && val2 > val3 && val2 > val4 && val2 > val5)
+	{
+		std::cout << "Your largest number is: " << val2;
+	}
+	if (val3 > val1 && val3 > val2 && val3 > val4 && val3 > val5)
+	{
+	std::cout << "Your largest number is: " << val3;
+	}
+	if (val4 > val1 && val4 > val2 && val4 > val3 && val4 > val5)
+	{
+	std::cout << "Your largest number is: " << val4;
+	}
+	if (val5 > val1 && val5 > val2 && val5 > val3 && val5 > val4)
+	{
+		std::cout << "Your largest number is: " << val5;
+	}
+}
